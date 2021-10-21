903440e079b3c957572c8b907f7f6d1e92e84888
savio <-@gmail.com>
2021-10-21 17:29:07 +0400

    Format fix

--- CH4/reverseCipher.py
+++ CH4/reverseCipher.py
@@ -8,20 +8,20 @@ message = "This is string to be reversed, This is " + \
            "encryption we don't want"
 
 def reverseString(string):
-    lengthOfString = len(string) - 1
-    tempString = ""
-    while lengthOfString >= 0:
-        tempString = tempString + string[lengthOfString]
-        lengthOfString -= 1 
+  lengthOfString = len(string) - 1
+  tempString = ""
+  while lengthOfString >= 0:
+    tempString = tempString + string[lengthOfString]
+    lengthOfString -= 1 
 
-    return tempString
+  return tempString
 
 if __name__ == '__main__':
-    
-    print("[+] The reverse string is ")
-    print("\n-------------------------\n")
-    print(reverseString(message))
-    print("[+] The reverse string is ")
-    print("\n-------------------------\n")
-    print(reverseString(reverseString(message)))
+  
+  print("[+] The reverse string is ")
+  print("\n-------------------------\n")
+  print(reverseString(message))
+  print("[+] The reverse string is ")
+  print("\n-------------------------\n")
+  print(reverseString(reverseString(message)))
 
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ This is repo for X-Days of Code in Python
 
 ### Table of Content's 
 
-. CH4 -- Reverse Cipher
-. CH5 -- Caser Cipher
+* CH4 -- Reverse Cipher
+* CH5 -- Caser Cipher
